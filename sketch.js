2c53990cb54e89b5d0cd0696b8a351592686f660
ayan poddar <ayanpoddar9@gmail.com>
2020-12-14 00:44:35 +0530

    fix some issue and also add ui

--- sketch.js
+++ sketch.js
@@ -1,28 +1,38 @@
-let capture,x = 0,canvas;
+let capture,x = 0,canvas, snaps = false, recording;
 function setup() {
   canvas = createCanvas(640, 480);
   capture = createCapture(VIDEO);
   capture.size(640, 480);
   capture.hide();
+  recording= document.getElementById("recording");
 }
-
-function draw() {
-  //background(0);
-  capture.loadPixels();
-  // image(capture, 0,0, width, height);
-  var w = capture.width;
-  copy(capture, 0, x, w, 1, 0, x , w, 1);
-
+function snap(){
+  if(capture.pixels[1] > 0){
   
-  // stroke(51);
-  // strokeWeight(2);
-  // line(0, x, width, x);
-  x = x+1;
+    snaps = true;
+  }
+}
+function draw() {
+background(0);
+
+  capture.loadPixels();
+  if(capture.pixels[1] > 0){
+    if(snaps){
+    var w = capture.width;
+    copy(capture, 0, x, w, 1, 0, x , w, 1);
+
+    
+    // stroke(51);
+    // strokeWeight(2);
+    // line(0, x, width, x);
+    x = x+1;
 
     if(x > height){
       saveCanvas(canvas, "shot1", "png");
       noLoop();
-      alert();
-      //x = 0;
+      x = 0;
     }
+  }
+  recording.style.display = 'block';
+  }
 }
